af794b24cc688b6d2318b3920a26610fa9d645c9
fabianajara <fabianajara.u@gmail.com>
2024-10-25 19:38:07 -0600

    Update Usuario Model

--- PracticaProgramada2_Grupo8/Models/UsuarioModel.cs
+++ PracticaProgramada2_Grupo8/Models/UsuarioModel.cs
@@ -1,6 +1,16 @@
 ﻿namespace PracticaProgramada2_Grupo8.Models
 {
-    public class UsuariosModel
+    public class UsuarioModel
     {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+        public string email { get; set; }
+
+        public string telefono { get; set; }
+
+        public string contrasena { get; set; }
+
     }
 }
