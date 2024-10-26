338a46035991b166dcd4e3fd3b0104625cec45cd
JPabloBE <be.josepablo@gmail.com>
2024-10-25 20:11:00 -0600

    Modelo de canción.

---
+++ PracticaProgramada2_Grupo8/Models/CancionesModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PracticaProgramada2_Grupo8.Models
+{
+    public class CancionModel
+    {
+
+        public int id { get; set; }
+
+        public string titulo { get; set; }
+
+        public string artista { get; set; }
+
+    }
+}
+
